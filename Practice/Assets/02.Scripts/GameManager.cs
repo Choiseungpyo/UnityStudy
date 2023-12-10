093518d667d52457c71657e28c554409d8f47aa9
Choiseungpyo <86085827+Choiseungpyo@users.noreply.github.com>
2023-12-10 21:40:55 +0900

    Page 507 ~ 532
    싱글턴 패턴, 오브젝트 풀링, 교재 내 오류 발견 후 해결 완료(몬스터의 상태가 Die로 고정되어 있어 해당 몬스터가 꺼졌다 켜졌다하는 오류)

--- Practice/Assets/02.Scripts/GameManager.cs
+++ Practice/Assets/02.Scripts/GameManager.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     // 몬스터가 출현할 위치를 저장할 배열
     public List<Transform> points = new List<Transform>();
+
+    // 몬스터를 미리 생성해 저장할 리스트 자료형
+    public List<GameObject> monsterPool = new List<GameObject>();
+
+    // 오브젝트 풀(Object Pool)에 생성할 몬스터의 최대 개수
+    public int maxMonsters = 10;
 
     // 몬스터 프리팹을 연결할 변수
     public GameObject monster;
@@ -30,10 +37,34 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    // 싱글턴 인스턴스 선언
+    public static GameManager instance = null;
+
+    // 스크립트가 실행되면 가장 먼저 호출되는 유니티 이벤트 함수
+    private void Awake()
+    {
+        // instance가 할당되지 않았을 경우
+
+        if(instance == null)
+        {
+            instance = this;
+        }
+        // instance에 할당된 클래스의 인스턴스가 다를 경우 새로 생성된 클래스를 의미함
+        else if(instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+
+        // 다른 씬으로 넘어가더라도 삭제하지 않고 유지함
+        DontDestroyOnLoad(this.gameObject);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
+        // 몬스터 오브젝트 풀 생성
+        CreateMonsterPool();
+
         // SpawnPointGroup 게임오브젝트의 Transform 컴포넌트 추출
         Transform spawnPointGroup = GameObject.Find("SpawnPointGroup")?.transform;
 
@@ -51,7 +82,52 @@ public class GameManager : MonoBehaviour
     {
         // 몬스터의 불규칙한 생성 위치 산출
         int idx = Random.Range(0, points.Count);
+
         // 몬스터 프리팹 생성
-        Instantiate(monster, points[idx].position, points[idx].rotation);
+        //Instantiate(monster, points[idx].position, points[idx].rotation);
+
+        // 오브젝트 풀에서 몬스터 추출
+        GameObject _monster = GetMonsterInPool();
+        // 추출한 몬스터의 위치와 회전을 설정
+        _monster?.transform.SetPositionAndRotation(points[idx].position, points[idx].rotation);
+        // 추출한 몬스터를 활성화
+        _monster?.SetActive(true);
+        Debug.Log(_monster.name + " 생성");
+    }
+
+    // 오브젝트 풀에 몬스터 생성
+    void CreateMonsterPool()
+    {
+        for (int i = 0; i < maxMonsters; i++)
+        {
+            // 몬스터 생성
+            var _monster = Instantiate<GameObject>(monster);
+            // 몬스터의 이름을 지정
+            _monster.name = $"Monster_{i:00}";
+            // 몬스터 비활성화
+            _monster.SetActive(false);
+            Debug.Log("CreateMonsterPool 몬스터 비활성화");
+
+            // 생성한 몬스터를 오브젝트 풀에 추가
+            monsterPool.Add(_monster);
+        }
+    }
+
+    // 오브젝트 풀에서 사용 가능한 몬스터를 추출해 반환하는 함수
+    public GameObject GetMonsterInPool()
+    {
+        // 오브젝트 풀의 처음부터 끝까지 순회
+        foreach(var _monster in monsterPool)
+        {
+            // 비활성화 여부로 사용 가능한 몬스터를 판단
+            if(_monster.activeSelf == false)
+            {
+                // 몬스터 반환
+                Debug.Log("GetMonsterInPool : " + _monster.name);
+                return _monster;
+            }
+        }
+
+        return null;
     }
 }
